62010dfe5a17af97453b1333abdf662bc2b384b3
Gustavo Adolfo Rodriguez <1432287+garodriguezlp@users.noreply.github.com>
2021-07-26 21:04:30 -0500

    something that works

--- custom-container-descriptor-handler/src/main/java/com/garodriguezlp/ccdh/AssemblyDescriptorHandler.java
+++ custom-container-descriptor-handler/src/main/java/com/garodriguezlp/ccdh/AssemblyDescriptorHandler.java
@@ -2,18 +2,26 @@ package com.garodriguezlp.ccdh;
 
 import static com.fasterxml.jackson.databind.PropertyNamingStrategies.KEBAB_CASE;
 import static com.fasterxml.jackson.dataformat.yaml.YAMLGenerator.Feature.WRITE_DOC_START_MARKER;
+import static java.nio.file.Files.newInputStream;
+import static java.nio.file.Files.newOutputStream;
+import static org.apache.commons.compress.utils.IOUtils.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.jar.JarArchiveOutputStream;
 import org.apache.commons.io.IOUtils;
 import org.apache.maven.plugins.assembly.filter.ContainerDescriptorHandler;
 import org.codehaus.plexus.archiver.Archiver;
@@ -25,23 +33,42 @@ import org.codehaus.plexus.components.io.fileselectors.FileInfo;
 @Component(role = ContainerDescriptorHandler.class, hint = "assembly-descriptor")
 public class AssemblyDescriptorHandler implements ContainerDescriptorHandler {
 
+    // @todo: yaml inside the jar
+    // @todo: jar in the right location
+    // @todo: integration testing
+
     private static final String YAML = "foo.yaml";
+    private static final String DESCRIPTOR_LOCATION = "lib/foo";
+
     private List<AssemblyEntry> entries = new ArrayList<>();
 
     @Override
     public void finalizeArchiveCreation(Archiver archiver) throws ArchiverException {
         archiver.getResources().forEachRemaining(a -> {
+            System.out.println("a = " + a.getName());
         }); // necessary to prompt the isSelected() call
 
         File descriptor;
         try {
-            descriptor = Files.createTempFile(YAML, ".tmp").toFile();
-            createYamlDescriptor(entries, descriptor);
+            descriptor = Files.createTempFile("descriptor-jar", ".tmp").toFile();
+            descriptor.deleteOnExit();
+
+            File yamlDescriptor = createYamlDescriptor(entries);
+            buildDescriptorArchive(yamlDescriptor, descriptor);
+
+            String descriptorDestPath = buildDescriptorDestPath(entries.get(0).getName());
+            archiver.addFile(descriptor, descriptorDestPath);
+
         } catch (IOException e) {
             throw new ArchiverException("Error...", e);
         }
 
-        archiver.addFile(descriptor, YAML);
+    }
+
+    private String buildDescriptorDestPath(String someAssemblyEntry) {
+        String assemblyRootDir = someAssemblyEntry.substring(0, someAssemblyEntry.indexOf('/'));
+        String jarDescriptorFinalName = assemblyRootDir + ".jar";
+        return String.join("/", assemblyRootDir, DESCRIPTOR_LOCATION, jarDescriptorFinalName);
     }
 
     @Override
@@ -50,12 +77,12 @@ public class AssemblyDescriptorHandler implements ContainerDescriptorHandler {
 
     @Override
     public List<String> getVirtualFiles() {
-        return Arrays.asList(YAML);
+        return Collections.singletonList(YAML);
     }
 
     @Override
     public boolean isSelected(FileInfo fileInfo) throws IOException {
-        if (!fileInfo.isDirectory()){
+        if (!fileInfo.isDirectory()) {
             byte[] contents = IOUtils.toByteArray(fileInfo.getContents());
             String sha256 = DigestUtils.sha256Hex(contents);
             entries.add(new AssemblyEntry(fileInfo.getName(), contents.length, sha256));
@@ -63,9 +90,11 @@ public class AssemblyDescriptorHandler implements ContainerDescriptorHandler {
         return true;
     }
 
-
-    private void createYamlDescriptor(List<AssemblyEntry> assemblyEntries, File resultFile) throws IOException {
-        buildYamlObjectMapper().writeValue(resultFile, assemblyEntries);
+    private File createYamlDescriptor(List<AssemblyEntry> assemblyEntries) throws IOException {
+        File resultYaml = Files.createTempFile("descriptor.yaml", ".tmp").toFile();
+        resultYaml.deleteOnExit();
+        buildYamlObjectMapper().writeValue(resultYaml, assemblyEntries);
+        return resultYaml;
     }
 
     private ObjectMapper buildYamlObjectMapper() {
@@ -73,5 +102,18 @@ public class AssemblyDescriptorHandler implements ContainerDescriptorHandler {
                 .setPropertyNamingStrategy(KEBAB_CASE)
                 .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .registerModule(new JavaTimeModule());
+    }
+
+    private void buildDescriptorArchive(File yamlDescriptor, File descriptorJar) throws IOException {
+        try (JarArchiveOutputStream jarOutStream =
+                     new JarArchiveOutputStream(new BufferedOutputStream((newOutputStream(descriptorJar.toPath()))))) {
+            ArchiveEntry entry = jarOutStream.createArchiveEntry(yamlDescriptor, YAML);
+            jarOutStream.putArchiveEntry(entry);
+            try (InputStream yamlDescriptorInputStream = new BufferedInputStream(newInputStream(yamlDescriptor.toPath()))) {
+                copy(yamlDescriptorInputStream, jarOutStream);
+            }
+            jarOutStream.closeArchiveEntry();
+            jarOutStream.finish();
+        }
     }
 }
